a087dbc28b188d282efe39ffcf136cf452917a25
TheSoftNode <thesoftnode@gmail.com>
2023-12-12 15:11:58 +0100

    Added redis for caching and cloudinary for image store

---
+++ server/dataAccess/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+// Cloudinary config
+export const cloudImageStore = () =>
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET_KEY,
+  });
