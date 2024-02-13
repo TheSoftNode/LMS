bc51987ae5905d79fce70c825d38f286e45aa898
TheSoftNode <thesoftnode@gmail.com>
2024-02-13 17:21:42 +0100

    Concluded the Create course UI

--- client-side/app/components/Admin/Course/CreateCourse.tsx
+++ client-side/app/components/Admin/Course/CreateCourse.tsx
@@ -5,6 +5,7 @@ import CourseInformation from "./CourseInformation";
 import CourseOptions from "./CourseOptions";
 import CourseData from "./CourseData";
 import CourseContent from "./CourseContent";
+import { Description } from "@mui/icons-material";
 
 type Props = {};
 
@@ -35,7 +36,52 @@ const CreateCourse = (props: Props) => {
 
   const [courseData, setCourseData] = useState({});
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async () => {
+    // Format benefits array
+    const formattedBenefits = benefits.map((benefit) => ({
+      title: benefit.title,
+    }));
+
+    // Format prerequisites array
+    const formattedPrerequisites = prerequisites.map((prerequisite) => ({
+      title: prerequisite.title,
+    }));
+
+    // Format course content array
+    const formattedCourseContentData = courseContentData.map(
+      (courseContent) => ({
+        videoUrl: courseContent.videoUrl,
+        title: courseContent.title,
+        description: courseContent.description,
+        videoSection: courseContent.videoSection,
+        links: courseContent.links.map((link) => ({
+          title: link.title,
+          url: link.url,
+        })),
+        suggestion: courseContent.suggestion,
+      })
+    );
+
+    // Prepare our data object
+    const data = {
+      name: courseInfo.name,
+      description: courseInfo.description,
+      price: courseInfo.price,
+      estimatedPrice: courseInfo.estimatedPrice,
+      tags: courseInfo.tags,
+      thumbnail: courseInfo.thumbnail,
+      level: courseInfo.level,
+      demoUrl: courseInfo.demoUrl,
+      totalVideos: courseContentData.length,
+      benefits: formattedBenefits,
+      prerequisites: formattedPrerequisites,
+      courseContent: formattedCourseContentData,
+    };
+
+    setCourseData(data);
+  };
+
+  console.log(courseData);
 
   return (
     <div className="w-full flex min-h-screen">
