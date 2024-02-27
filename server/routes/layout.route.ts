d57a8421cff949c0487a1d5b105ea6baa757f34f
TheSoftNode <thesoftnode@gmail.com>
2024-02-27 11:38:35 +0100

    Add the getLayoutByType route

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,16 +1,20 @@
 import express from "express";
-import { createLayout, editLayout } from "../controllers/layout.controller";
+import {
+  createLayout,
+  editLayout,
+  getLayoutByType,
+} from "../controllers/layout.controller";
 import { restrictTo } from "../middlewares/roleManager2";
 import { isAuthenticated } from "../middlewares/protectRoute2";
 
 const router = express.Router();
 
-router
-  .route("/create-layout")
-  .post(isAuthenticated, restrictTo("admin"), createLayout);
+router.use(isAuthenticated, restrictTo("admin"));
 
-router
-  .route("/update-layout")
-  .put(isAuthenticated, restrictTo("admin"), editLayout);
+router.route("/create-layout").post(createLayout);
+
+router.route("/update-layout").put(editLayout);
+
+router.route("/get-layout/:type").get(getLayoutByType);
 
 export default router;
