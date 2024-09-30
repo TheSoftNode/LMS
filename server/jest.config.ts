345ff10d97b09a59a607b6286cd145e9cf9135d3
TheSoftNode <thesoftnode@gmail.com>
2024-09-30 04:40:47 +0100

    set up the tsconfig and jestconfig files

---
+++ server/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    verbose: true,
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+    },
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    testEnvironment: 'node',
+};
+
+
+export default config;
