300709825ab69061be53a28c2cbcf3f079327e95
TheSoftNode <thesoftnode@gmail.com>
2024-09-30 01:55:04 +0100

    Add swagger docs for users management and authenticatiion endpoints

--- server/startUps/app.ts
+++ server/startUps/app.ts
@@ -1,21 +1,30 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-// import rateLimit from "express-rate-limit";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
 import helmet from "helmet";
 import { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "../errorsHandlers/errorHandler";
 import { mountedRoutes } from "./routes";
-
-// import userRouter from "../routes/user.route";
-// import courseRouter from "../routes/course.route";
-// import courseDataRouter from "../routes/courseData.route";
-// import commentRouter from "../routes/comment.route";
+import { readFileSync } from "fs";
 
 const app = express();
 
-// Always put at the top of all middlewares
+const __dirname = path.resolve();
+
+
+// Read Swagger JSON files
+const swaggerDocument = JSON.parse(
+  readFileSync(path.join(__dirname, './swagger/swagger.json'), 'utf8')
+);
+
+// Swagger route for users
+app.use('/LMS', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 // SET Security HTTP headers
 app.use(helmet());
 
@@ -23,12 +32,12 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 // Limit request from the same IP address
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: "Too many requests from this IP. Please try again in an hour",
-// });
-// app.use("/api", limiter);
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. Please try again in an hour",
+});
+app.use("/api", limiter);
 
 // The body parser, reading data from the body into req.body
 app.use(express.json({ limit: "10kb" }));
@@ -46,16 +55,12 @@ app.use(corOptions);
 
 mountedRoutes(app);
 
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/courses", courseRouter);
-// app.use("/api/v1/courseData", courseDataRouter);
-// app.use("/api/v1/comments", commentRouter);
 
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
+app.all("*", (req: Request, res: Response, next: NextFunction) =>
+{
   const err: any = new Error(`Can't find ${req.originalUrl} in this server!`);
   err.statusCode = 404;
   next(err);
-  // next(new AppError(`Can't find ${req.originalUrl} in this server!`, 404));
 });
 
 app.use(globalErrorHandler);
